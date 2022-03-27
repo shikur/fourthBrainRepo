45afc4eab62c2fb09e5cc8f4d8abbf1d46684175
shikur <dangtech2020@gmail.com>
2022-03-26 20:42:49 -0700

    code for data and load csv file

--- src/Helloworld.py
+++ src/Helloworld.py
@@ -1,1 +1,4 @@
-print("this is test to work with")
+import pandas as pd
+
+df = pd.read_csv('/home/shikur/notebookProject/MLOPs4thClass/fourthBrainRepo/data/csvdata/annual2020finalcialyear.csv')
+print(df.head())
